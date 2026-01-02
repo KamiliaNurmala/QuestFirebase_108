efcd40ee51e394f6bf411a6cb6d9781b076451b3
KamiliaNurmala <nurmalakamilia@gmail.com>
2026-01-02 18:20:24 +0700

    add navigation argument support to DestinasiDetail

--- app/src/main/java/com/example/pertemuan11/view/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/pertemuan11/view/route/DestinasiDetail.kt
@@ -4,4 +4,7 @@ import com.example.pertemuan11.view.route.DestinasiNavigasi
 object DestinasiDetail : DestinasiNavigasi {
     override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
