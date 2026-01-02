f1f578bbd331b06c41657ae86993e52b1dd1ffe3
KamiliaNurmala <nurmalakamilia@gmail.com>
2026-01-02 18:27:42 +0700

    define DestinasiEntry for student entry navigation

---
+++ app/src/main/java/com/example/pertemuan11/view/route/DestinasiEntry.kt
@@ -0,0 +1,7 @@
+import com.example.pertemuan11.R
+import com.example.pertemuan11.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
