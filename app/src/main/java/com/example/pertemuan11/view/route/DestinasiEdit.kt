665b475fded6c09fa3202281a0cd8842ac00513e
KamiliaNurmala <nurmalakamilia@gmail.com>
2026-01-02 18:24:10 +0700

    implement DestinasiEdit with navigation arguments

---
+++ app/src/main/java/com/example/pertemuan11/view/route/DestinasiEdit.kt
@@ -0,0 +1,10 @@
+import com.example.pertemuan11.R
+import com.example.pertemuan11.view.route.DestinasiNavigasi
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
