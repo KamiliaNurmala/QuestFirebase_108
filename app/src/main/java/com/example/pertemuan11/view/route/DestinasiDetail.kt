d7463a3266d912123c1a00b76087cfe9ab1cd6a9
KamiliaNurmala <nurmalakamilia@gmail.com>
2026-01-02 18:18:19 +0700

    define DestinasiDetail object and basic route

---
+++ app/src/main/java/com/example/pertemuan11/view/route/DestinasiDetail.kt
@@ -0,0 +1,7 @@
+import com.example.pertemuan11.R
+import com.example.pertemuan11.view.route.DestinasiNavigasi
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+}
