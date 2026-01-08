3d88016edf468b521759c15d7fe28be0e1eb0c5c
KamiliaNurmala <nurmalakamilia@gmail.com>
2026-01-08 17:32:22 +0700

    membuat sealed interface untuk status UI dan properti dasar di ViewMode

--- app/src/main/java/com/example/pertemuan11/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/viewmodel/DetailViewModel.kt
@@ -1,2 +1,34 @@
+@file:OptIn(InternalSerializationApi::class)
+
 package com.example.pertemuan11.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan11.modeldata.Siswa
+import com.example.pertemuan11.repositori.RepositorySiswa
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+}
