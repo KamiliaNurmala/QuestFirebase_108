9856717551eb9ccf7e776caf15e3041ddd6a66bf
KamiliaNurmala <nurmalakamilia@gmail.com>
2026-01-08 17:59:31 +0700

    Menambahkan fungsi untuk memperbarui data di dalam UI State

--- app/src/main/java/com/example/pertemuan11/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/viewmodel/EditViewModel.kt
@@ -31,4 +31,17 @@ class EditViewModel(
                 .toUiStateSiswa(true)
         }
     }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
 }
