988fdef411b89f79269a4c0a908f44e0e8b73476
KamiliaNurmala <nurmalakamilia@gmail.com>
2026-01-08 17:53:35 +0700

    membuat struktur dasar class, pengambilan ID dari navigasi, dan memuat data siswa yang akan diedit ke dalam UI State

---
+++ app/src/main/java/com/example/pertemuan11/viewmodel/EditViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.pertemuan11.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan11.modeldata.DetailSiswa
+import com.example.pertemuan11.modeldata.UIStateSiswa
+import com.example.pertemuan11.modeldata.toDataSiswa
+import com.example.pertemuan11.modeldata.toUiStateSiswa
+import com.example.pertemuan11.repositori.RepositorySiswa
+import com.example.pertemuan11.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+}
